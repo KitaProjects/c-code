eb58655359660f8e28042ec0d11d7ef0a31b28f3
Killian Carolan <xriss_xross@outlook.com>
2026-01-29 11:24:07 +0000

    Hello world

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// define a constant NO = SIGN
+#define PI 3.14
+
+int main() {
+	printf("Hello world!\n");
+
+	// a will be assigned to some rubbish like 50501405 but compiler won't complain
+	int r = 2, a;
+	a = PI * (r * r);
+	
+	printf("The radius of the circle is %d.\n", r);
+	printf("The area of the circle is %d.\n", a);
+
+	return 0;
+}
