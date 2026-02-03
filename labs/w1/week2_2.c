d90760cab19831e192014a2d6427e1ff861a0427
Killian Carolan <xriss_xross@outlook.com>
2026-02-03 15:56:30 +0000

    Improved w2.2 solution

--- labs/w1/week2_2.c
+++ labs/w1/week2_2.c
@@ -4,18 +4,18 @@ int main(void) {
 	int hi, med, lo, mark;
 	hi = med = lo = 0;
 
-	scanf("%i", &mark);
-	while (mark != 0) {
+	do {
+		scanf("%i", &mark);
+
 		if (mark >= 85) {
 			hi++;
 		} else if (mark >= 60) {
 			med++;
-		} else {
+		} else if (mark != 0) {
 			lo++;
 		}
 
-		scanf("%i", &mark);
-	};
+	} while (mark != 0);
 
 	printf(
 		">=85:%i\n"
