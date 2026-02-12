6c7dffefbaf21ec735e706cae582bcb1f08c1490
Killian Carolan <xriss_xross@outlook.com>
2026-02-12 11:20:37 +0000

    DONE: Assignment 1_1

---
+++ assignments/1/assignment1_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+int main(void) {
+	int r, c1, c2;
+	
+	float area, circ, squareArea;
+	float totalArea = 0, totalCirc = 0, totalSquareArea = 0;
+
+	/* c1 and c2 are the radius of two circles */
+	scanf("%d%d", &c1, &c2);
+
+	for (r=c1; r<=c2; r++) {
+		area = PI * (r * r);
+		totalArea += area;
+
+		circ = 2 * PI * r;
+		totalCirc += circ;
+
+		squareArea = 2 * (r * r);
+		totalSquareArea += squareArea;
+	}
+
+	printf("%.3f\n%.3f\n%.3f\n", totalArea, totalCirc, totalSquareArea);
+
+	return 0;
+}
