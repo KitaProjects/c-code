8aa3e26ba27156aa9d0e4d83191d61657c60ba3c
Killian Carolan <xriss_xross@outlook.com>
2026-02-13 17:41:08 +0000

    DONE: Assignment1_5

--- assignments/1/assignment1_5.c
+++ assignments/1/assignment1_5.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
 
 int main(void) {
-	char a[100], b[100];
+	/* assuming that '\0' is included in this count */
+	char inp_a[101], inp_b[101], result[102];
 
-	scanf("%s\n%s", a, b);
+	scanf("%s\n%s", inp_a, inp_b);
+
+	/* size of inputs in digits */
+	int i_a = 0, i_b = 0, i_r = 0, carry = 0;
+	while (inp_a[i_a] != '\0') i_a++;
+	while (inp_b[i_b] != '\0') i_b++;
+
+	/* TODO: looks ugly find cleaner way of doing this */
+	i_a--;
+	i_b--;
+
+	int sum;
+	while(i_a >= 0 || i_b >= 0 || carry > 0) {
+		int digit_a = 0, digit_b = 0;
+
+		/* NOTE: ternary operators would be better here */
+		if (i_a >= 0) {
+			digit_a = inp_a[i_a] - '0';
+		}
+		if (i_b >= 0) {
+			digit_b = inp_b[i_b] - '0';
+		}
+
+		sum = digit_a + digit_b + carry;
+
+		result[i_r] = (sum % 10) + '0';
+		carry = sum / 10;
+
+		i_a--;
+		i_b--;
+		i_r++;
+	}
+
+	/* reverse print backwards stored result[] */
+	for (i_r-- ; i_r >= 0; i_r--) {
+		printf("%c", result[i_r]);
+	}
+	printf("\n");
 
 	return 0;
 }
